52ecfb0d720ecb032ff4337e7453d3bf7efd21ff
Alex Galvis <alex.galvis.sistemas@gmail.com>
2021-12-14 12:54:24 -0500

    Update project structure

---
+++ src/main/java/com/gsdd/pcstock/util/DetailedFileDirectoryHelper.java
@@ -0,0 +1,50 @@
+package com.gsdd.pcstock.util;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.apache.commons.io.FileUtils;
+import com.gsdd.pcstock.model.DetailedFile;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Setter
+@Getter
+@Slf4j
+public class DetailedFileDirectoryHelper extends AbstractDirectoryHelper<DetailedFile> {
+
+  private boolean withResolution;
+
+  @Override
+  public List<DetailedFile> traverseDirectories(String[] filters, List<File> directories) {
+    List<DetailedFile> fileList = new CopyOnWriteArrayList<>();
+    directories.parallelStream().forEach(
+        directory -> FileUtils.listFiles(directory, filters, true).stream().forEach(file -> {
+          log.debug("fp > {}", file.getAbsolutePath());
+          fileList.add(DetailedFile.builder().name(file.getName()).size(file.length())
+              .resolution(extractResolution(file.getAbsolutePath())).build());
+        }));
+    Collections.sort(fileList);
+    return fileList;
+  }
+
+  private String extractResolution(String path) {
+    String resolucion = "0x0";
+    if (withResolution) {
+      try {
+        resolucion = VideoPropertiesUtil.getFileResolutionXuggler(path);
+      } catch (Exception e) {
+        log.warn("Error during extract resolution for: {}", path, e);
+      }
+    }
+    return resolucion;
+  }
+
+  @Override
+  public Long extractAccountField(DetailedFile pcStockFile) {
+    return pcStockFile.getSize();
+  }
+
+}
